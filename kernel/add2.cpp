c4657a7337cf2db6285a53e9f3ac921a9008cf2b
weiyang.god <weiyang.god@bytedance.com>
2021-03-20 00:15:49 +0800

    add TORCH_LIBRARY interface

--- kernel/add2.cpp
+++ kernel/add2.cpp
@@ -4,7 +4,7 @@
 void torch_launch_add2(torch::Tensor &c,
                        const torch::Tensor &a,
                        const torch::Tensor &b,
-                       int n) {
+                       int64_t n) {
     launch_add2((float *)c.data_ptr(),
                 (const float *)a.data_ptr(),
                 (const float *)b.data_ptr(),
@@ -15,4 +15,8 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("torch_launch_add2",
           &torch_launch_add2,
           "add2 kernel warpper");
+}
+
+TORCH_LIBRARY(add2, m) {
+    m.def("torch_launch_add2", torch_launch_add2);
 }
